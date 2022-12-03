834a58ff1041d5359bd5bc4d10d34cdbb24d7e40
Yura1979 <tyi@outlook.com>
2022-12-03 12:57:52 +0200

    GitLab group and project config

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Configure the GitLab Provider
+provider "gitlab" {
+  base_url = var.gitlab_url
+  token    = var.gitlab_token
+}
+
+# Add a group
+resource "gitlab_group" "gitlab_wa_hw03_group" {
+  name        = "wa-${var.gitlab_group_name}"
+  path        = "wa-${var.gitlab_group_name}"
+  description = "An wa-${var.gitlab_group_name}} group"
+  visibility_level = "internal"
+}
+
+# Add a project to the group - example/example
+resource "gitlab_project" "gitlab_wa_hw03_project" {
+  name         = var.gitlab_project_name
+  namespace_id = gitlab_group.gitlab_wa_hw03_group.id
+
+  depends_on = [
+    gitlab_group.gitlab_wa_hw03_group
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "gitlab_url" {
+  description = "GitLab Server URL"
+  type = string
+  default = "https://gitlab.tyi.name/api/v4/"
+}
+variable "gitlab_token" {
+    type = string
+    description = "GitLab Server Tocken"
+    default = "glpat-_zANNckKK6vYU732kNHh"
+    sensitive = true
+}
+variable "gitlab_group_name" {
+  type = string
+  description = "GitLab Group name"
+  default = "101122"
+}
+variable "gitlab_project_name" {
+    type = string
+    description = "Gitlab Project Name"
+    default = "terraform_hw03"
+}
