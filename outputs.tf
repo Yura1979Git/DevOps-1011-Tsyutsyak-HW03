d42522c5bcdead4d537f786a2a4c7e4669b04c44
Yura1979Git <tyi+git@outlook.com>
2022-12-05 08:50:39 +0200

    Edit outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "gitlab_group" {
     value = gitlab_group.gitlab_wa_hw03_group.full_path
+    description = "Gitlab Group"
 }
 
 output "gitlab_project" {
     value = gitlab_project.gitlab_wa_hw03_project.name
+    description = "Gitlab Project"
 }
